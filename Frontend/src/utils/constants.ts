48b4a10e55191d92b10db29b0b700b10b27be1cf
Chetan Gupta <chetangupta2708@gmail.com>
2025-10-30 04:47:41 +0530

    fix : added a new backend url

--- Frontend/src/utils/constants.ts
+++ Frontend/src/utils/constants.ts
@@ -2,5 +2,5 @@ export  const exercisedburl = "https://exercisedb.p.rapidapi.com";
 export const youtubeSearchUrl ="https://youtube-search-and-download.p.rapidapi.com";
 
 
-export const baseURL = "https://fitcraft-hub.onrender.com/api/v1"
+export const baseURL = "https://fitcraft-hub-1.onrender.com/api/v1"
 // "http://localhost:5000/api/v1";
